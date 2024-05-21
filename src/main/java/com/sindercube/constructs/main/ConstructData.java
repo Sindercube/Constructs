432a316f130f0b80a84d99871392c01e1733182a
Soumeh <soumeh@proton.me>
2024-05-21 13:02:35 +0200

    more pattern types + other things

--- src/main/java/com/sindercube/constructs/main/ConstructData.java
+++ src/main/java/com/sindercube/constructs/main/ConstructData.java
@@ -1,24 +1,55 @@
 package com.sindercube.constructs.main;
 
+import com.google.common.collect.ImmutableMap;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
-import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.pattern.BlockPattern;
-import net.minecraft.block.pattern.BlockPatternBuilder;
-import net.minecraft.block.pattern.CachedBlockPosition;
 import net.minecraft.entity.EntityType;
 import net.minecraft.registry.Registries;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3i;
 
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ConstructData {
 
+	private static Character fromString(String string) {
+		return string.charAt(0);
+	}
+
+	private static final Codec<Character> CHAR_CODEC = Codec.STRING.xmap(ConstructData::fromString, Object::toString);
+	private static final Codec<Map<Character, Blockish>> KEY_CODEC = Codec.unboundedMap(CHAR_CODEC, Blockish.CODEC);
+
+	public static final Codec<ConstructData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+			StringPattern.CODEC.fieldOf("pattern").forGetter(null),
+			KEY_CODEC.fieldOf("key").forGetter(null),
+			Vec3i.CODEC.fieldOf("offset").forGetter(null),
+			Registries.ENTITY_TYPE.getCodec().fieldOf("entity").forGetter(null)
+	).apply(instance, ConstructData::new));
+
 	private final BlockPattern pattern;
 	private final Vec3i offset;
 	private final EntityType<?> entity;
+
+	public ConstructData (
+			StringPattern pattern,
+			Map<Character, Blockish> keys,
+			Vec3i offset,
+			EntityType<?> entity
+	) {
+		keys = modifyKeys(keys);
+		this.pattern = pattern.generateBlockPattern(keys);
+		this.offset = offset;
+		this.entity = entity;
+	}
+
+	public Map<Character, Blockish> modifyKeys(Map<Character, Blockish> keys) {
+		var copy = new HashMap<>(keys);
+		copy.put(' ', new Blockish.BlockEntry(Blocks.AIR));
+		return ImmutableMap.copyOf(copy);
+	}
 
 	public BlockPattern getPattern() {
 		return pattern;
@@ -31,38 +62,5 @@ public class ConstructData {
 	public BlockPos offsetPosition(BlockPattern.Result result) {
 		return result.translate(offset.getX(), offset.getY(), offset.getZ()).getBlockPos();
 	}
-
-	public ConstructData (
-			List<String> pattern,
-			Map<String, Blockish> keys,
-			Vec3i offset,
-			EntityType<?> entity
-	) {
-		this.pattern = fromData(pattern, keys);
-		this.offset = offset;
-		this.entity = entity;
-	}
-
-	public static BlockPattern fromData(List<String> pattern, Map<String, Blockish> keys) {
-		var builder = BlockPatternBuilder.start();
-		builder = builder.aisle(pattern.toArray(String[]::new));
-		for (String key : keys.keySet()) {
-			var blockish = keys.get(key);
-			builder.where(key.charAt(0), CachedBlockPosition.matchesBlockState(blockish.match()));
-		}
-		// TODO maybe one day make it ignore blocks around it
-//		builder.where(' ', p -> true);
-		builder.where(' ', CachedBlockPosition.matchesBlockState(AbstractBlock.AbstractBlockState::isAir));
-		return builder.build();
-	}
-
-	private static final Codec<Map<String, Blockish>> KEY_CODEC = Codec.unboundedMap(Codec.STRING, Blockish.CODEC);
-
-	public static final Codec<ConstructData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-			Codec.STRING.listOf().fieldOf("pattern").forGetter(null),
-			KEY_CODEC.fieldOf("key").forGetter(null),
-			Vec3i.CODEC.fieldOf("offset").forGetter(null),
-			Registries.ENTITY_TYPE.getCodec().fieldOf("entity").forGetter(null)
-	).apply(instance, ConstructData::new));
 
 }
